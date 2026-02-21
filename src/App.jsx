22e0b717d4eb06e47ccdb82c8a10efe1e596c2d0
TemurMalik <tmaq77@gmail.com>
2026-02-21 14:40:10 +0500

    Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import { signInWithPopup, signOut } from "firebase/auth";
+import { auth, provider } from "./firebase";
 
 function App() {
+  let LoginWithGoogle = async () => {
+    try {
+      let res = await signInWithPopup(auth, provider);
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <h1>App</h1>
+      <button>
+        <span onClick={LoginWithGoogle}>Login with Google</span>
+      </button>
     </>
   );
 }
