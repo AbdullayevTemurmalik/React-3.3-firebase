d3aa0332fb8fbc7ac5d8cbcbb36a7d7dea6349fb
TemurMalik <tmaq77@gmail.com>
2026-02-21 14:16:16 +0500

    Firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6WBrFwkcskspXQ7UnBZ6Ki4OfaTnfQ1Q",
+  authDomain: "exclusive-shop-project.firebaseapp.com",
+  projectId: "exclusive-shop-project",
+  storageBucket: "exclusive-shop-project.firebasestorage.app",
+  messagingSenderId: "470642496563",
+  appId: "1:470642496563:web:1a108961dfb86960923f46",
+  measurementId: "G-K17G760ETL",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
