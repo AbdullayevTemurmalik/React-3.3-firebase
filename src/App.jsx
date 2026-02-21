493316aa309b8140727b053e7c6890b91f4df147
TemurMalik <tmaq77@gmail.com>
2026-02-21 14:42:22 +0500

    Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <button>
-        <span onClick={LoginWithGoogle}>Login with Google</span>
+        <span onClick={LoginWithGoogle}>Sing with Google</span>
       </button>
     </>
   );
